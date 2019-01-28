db693725ec8b08f03a0552cbb8fad9f219ad9f7d
Michal Gáborík <michal.gaborik.77@gmail.com>
2019-01-28 01:50:52 +0100

    added some shity animation on fragment transaction for test

--- app/src/main/java/com/example/kointest/core/extentions/FragmentTransactionExtention.kt
+++ app/src/main/java/com/example/kointest/core/extentions/FragmentTransactionExtention.kt
@@ -1,9 +1,11 @@
 package com.example.kointest.core.extentions
 
 import androidx.fragment.app.FragmentTransaction
+import com.example.kointest.R
 import com.example.kointest.core.base.BaseFragment
 
 fun <T: BaseFragment> FragmentTransaction.simpleReplace(frameLayoutID: Int, fragment: T, addToBackstack: Boolean) {
+    setCustomAnimations(R.anim.abc_slide_in_top, R.anim.abc_popup_exit)
     replace(frameLayoutID, fragment)
     if (addToBackstack) addToBackStack(null)
     commit()
